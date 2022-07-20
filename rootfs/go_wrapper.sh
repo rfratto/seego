697bd4046e30bf1b823738dc30f56c9572f8da9c
Robert Fratto <robertfratto@gmail.com>
2022-07-20 09:25:47 -0400

    Revert "stop changing gocache values"
    This reverts commit a3c51557bf96632859005b14b4a2c864448d21eb.

--- rootfs/go_wrapper.sh
+++ rootfs/go_wrapper.sh
@@ -59,6 +59,8 @@ main() {
     export LD_LIBRARY_PATH="$FOUND_LD_PATH:$LD_LIBRARY_PATH"
   fi
 
+  export GOCACHE=$(pwd)/.cache
+
   exec sudo -E \
     --preserve-env=PATH \
     --preserve-env=LD_LIBRARY_PATH \
